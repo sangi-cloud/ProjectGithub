863d85511b866651e58030c137cde65a291f0384
Sarojini1999 <sarojinirajarethinam@gmail.com>
2022-09-06 21:27:09 +0530

    Saro code

--- src/test/java/org/test/github/Sample.java
+++ src/test/java/org/test/github/Sample.java
@@ -7,7 +7,7 @@ public class Sample {
 		System.out.println("Test 2");
 		System.out.println("Test 3");
 		System.out.println("Test 4");
-		System.out.println("Test 5");
+	
 		
 	}
 }
--- src/test/java/org/test/github/SampleGit.java
+++ src/test/java/org/test/github/SampleGit.java
@@ -6,7 +6,6 @@ public class SampleGit {
 		
 		System.out.println("Test 1");
 		System.out.println("Test 2");
-		System.out.println("Test 3");
 		
 		}
 
