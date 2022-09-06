ebb720535bf80a0a3c4d3f0de7f43e380d0c4f63
Sadiq0310 <sadysmart03@gmail.com>
2022-09-06 10:12:08 +0530

    sady's code

--- src/test/java/org/test/github/SampleGit.java
+++ src/test/java/org/test/github/SampleGit.java
@@ -3,6 +3,11 @@ package org.test.github;
 public class SampleGit {
 	public static void main(String[] args) {
 		
-	}
+		
+		System.out.println("Test 1");
+		System.out.println("Test 2");
+		System.out.println("Test 3");
+		
+		}
 
 }
