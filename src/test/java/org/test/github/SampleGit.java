a388885e2782f0ec4f58c4a80b350a435415e23f
Naveen1483 <amstvpn@gmail.com>
2022-09-06 19:28:17 +0530

    Naveen's code

--- src/test/java/org/test/github/SampleGit.java
+++ src/test/java/org/test/github/SampleGit.java
@@ -4,9 +4,9 @@ public class SampleGit {
 	public static void main(String[] args) {
 		
 		
-		System.out.println("Test 1");
-		System.out.println("Test 2");
-		System.out.println("Test 3");
+		System.out.println("Test 5");
+		System.out.println("Test 6");
+		System.out.println("Test 7");
 		
 		}
 
