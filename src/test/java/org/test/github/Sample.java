b03b7e9941854ae460ed0d38a02528857153a005
priya <gejapriya555@gmail.com>
2022-09-09 14:56:08 +0530

    priya's code

--- src/test/java/org/test/github/Sample.java
+++ src/test/java/org/test/github/Sample.java
@@ -8,6 +8,8 @@ public class Sample {
 		System.out.println("Test 3");
 		System.out.println("Test 4");
 		System.out.println("Test 5");
+		System.out.println("Test 7");
+		System.out.println("Test 8");
 		
 	}
 }
